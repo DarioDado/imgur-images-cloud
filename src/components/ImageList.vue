a0b0d5f2d40f78c6c7e61fda1bc2c9fec448c270
Dario <you@example.com>
2018-09-12 16:10:07 +0200

    add gallery grid style

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="images-container">
     <img v-for="image in allImages" :src="image.link" :key="image.id">
   </div>
 </template>
@@ -19,4 +19,16 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .images-container {
+    column-count: 3;
+    column-gap: 0;
+  }
+  img {
+    max-width: 100%;
+    padding: 5px;
+  }
+</style>
+
 
