7ffce47c085f9274ca61616ba091a149c2d4ddbb
Dario <you@example.com>
2018-09-12 14:13:55 +0200

    add fetchImages helper method to api imgur.js, create images vuex module and connect it to Image List component

--- src/api/imgur.js
+++ src/api/imgur.js
@@ -1,4 +1,5 @@
 import qs from 'qs';
+import Axios from 'axios';
 const CLIENT_ID = '9d9bf7f523254c8';
 const ROOT_URL = 'https://api.imgur.com';
 
@@ -9,5 +10,13 @@ export default {
       response_type: 'token',
     }
     window.location = `${ROOT_URL}/oauth2/authorize?${qs.stringify(querystring)}`;
-  }
+  },
+  async fetchImages(token) {
+    const response = await Axios.get(`${ROOT_URL}/3/account/me/images`,{
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data.data;
+  },
 }
--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
-    Images List
+    <img v-for="image in allImages" :src="image.link" :key="image.id">
   </div>
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex';
 export default {
   name: 'ImageList',
+  computed: {
+    ...mapGetters(['allImages']),
+  },
+  methods: {
+    ...mapActions(['fetchImages']),
+  },
+  created() {
+    this.fetchImages();
+  }
 }
 </script>
 
