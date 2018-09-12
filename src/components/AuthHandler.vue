9c3020054fa33b4bffaaed1269d013911b39a7c5
Dario <you@example.com>
2018-09-12 11:44:22 +0200

    create auth module finalizeLogin action

--- src/components/AuthHandler.vue
+++ src/components/AuthHandler.vue
@@ -5,8 +5,16 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 export default {
   name: 'AuthHandler',
+  methods: {
+    ...mapActions(['finalizeLogin']),
+  },
+  //vue lifecycle method created
+  created() {
+    this.finalizeLogin(window.location.hash);
+  }
 }
 </script>
 
--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,4 +1,5 @@
 import api from '../../api/imgur';
+import qs from 'qs';
 
 const state = {
   token: null,
@@ -11,6 +12,11 @@ const getters = {
 const actions = {
   login: () => {
     api.login();
+  },
+  finalizeLogin: ({ commit }, hash) => {
+    const queryStringParams = qs.parse(hash.replace('#',''));
+    const token = queryStringParams.access_token;
+    commit('setToken', token);
   },
   logout: ({ commit }) => {
     commit('setToken', null);
