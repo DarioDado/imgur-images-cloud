8668d1d39d038334eecd10d19149c769a7a985e9
Dario <you@example.com>
2018-09-12 14:37:46 +0200

    create upload-drag-and-drop-element

--- src/components/UploadForm.vue
+++ src/components/UploadForm.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    Upload Form
+    <div class="dropper">
+      <input type="file">
+      <span>Drag files here!</span>
+    </div>
   </div>
 </template>
 
@@ -9,3 +12,30 @@ export default {
   name: 'UploadForm',
 }
 </script>
+
+<style scoped>
+  .dropper {
+    height: 30vh;
+    border: 10px dashed #ddd;
+    border-radius: 15px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+  }
+  .dropper:hover {
+    background-color: #eee;
+  }
+  input {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    opacity: 0;
+  }
+  span {
+    font-size: 30px;
+    opacity: 0.7;
+    font-weight: bold;
+  }
+</style>
+
