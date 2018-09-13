47fc27b074daf3a61e469d7252b986b8003a1bf6
Dario <you@example.com>
2018-09-13 13:49:30 +0200

    create delete icon

--- src/components/ImageItem.vue
+++ src/components/ImageItem.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
+  <div class="image-wraper">
     <img :src="image.link" >
+    <i class="trash icon"></i>
   </div>
 </template>
 
@@ -17,6 +18,24 @@ export default {
   img {
     max-width: 100%;
     padding: 5px;
+  }
+  .image-wraper {
+    position: relative;
+  }
+  i {
+    position: absolute;
+    bottom: 20px;
+    right: 10px;
+    font-size: 20px;
+    color: tomato;
+    cursor: pointer;
+  }
+  i:hover {
+    font-size: 25px;
+  }
+  i {
+    -webkit-transition: 0.4s; /* Safari */
+    transition: 0.4s;
   }
 </style>
 
