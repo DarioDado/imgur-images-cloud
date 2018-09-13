222877bd9cc634ae26c4046b7491392ce1dd716b
Dario <you@example.com>
2018-09-13 09:07:44 +0200

    add production imgur api client id, deploy with firebase

--- src/api/imgur.js
+++ src/api/imgur.js
@@ -1,12 +1,13 @@
 import qs from 'qs';
 import Axios from 'axios';
 const CLIENT_ID = '9d9bf7f523254c8';
+const CLIENT_ID_PRODUCTION = '8ed73a6f840d318';
 const ROOT_URL = 'https://api.imgur.com';
 
 export default {
   login() {
     const querystring = {
-      client_id: CLIENT_ID,
+      client_id: CLIENT_ID_PRODUCTION,
       response_type: 'token',
     }
     window.location = `${ROOT_URL}/oauth2/authorize?${qs.stringify(querystring)}`;
